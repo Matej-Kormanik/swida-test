14c16eb1e5362fb5c5f64eb1711814e43deba4b0
Kormaník Matej <matej@kormanik.me>
2025-03-10 21:29:30 +0100

    Success banner.

--- vue-web/src/App.vue
+++ vue-web/src/App.vue
@@ -4,9 +4,16 @@
     import {ref} from "vue";
 
     const refreshTrigger = ref(0)
+    const showBanner = ref(false);
 
     const refreshOrders = () => {
       refreshTrigger.value++;
+      showBanner.value = true; // Zobrazíme banner
+
+      // Skryť banner po 3 sekundách
+      setTimeout(() => {
+        showBanner.value = false;
+      }, 3000);
     }
 </script>
 
@@ -14,6 +21,10 @@
 
 <template>
   <h1>Order management system</h1>
+  <div v-if="showBanner" class="banner">✅ Order successfully created!</div>
+
+
+
   <main>
     <OrderList :refresh="refreshTrigger"/>
     <OrderForm @orderCreated="refreshOrders"/>
@@ -27,8 +38,6 @@
 
 <style scoped>
   main {
-    max-width: 1280px;
-    margin: 2rem auto;
     display: flex;
     flex-direction: row;
     align-items: flex-start;
@@ -39,6 +48,16 @@
     margin-bottom: 3rem;
     padding-top: 2.6rem;
     font-weight: 500;
+  }
+  .banner {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px;
+    margin: 10px 0;
+    text-align: center;
+    border-radius: 5px;
+    font-weight: bold;
+    transition: opacity 0.5s;
   }
 
 </style>
